cfb4f299430c8b97527bf18557c238b6a7aadf54
Martin Kedmenec <martin.kedmenec@outlook.com>
2024-10-12 21:23:48 +0200

    Fix #29. Improve Testcontainers setup and tests

--- src/test/java/com/group6/novadashboardbackend/NovaDashboardBackEndApplicationTests.java
+++ src/test/java/com/group6/novadashboardbackend/NovaDashboardBackEndApplicationTests.java
@@ -1,56 +1,27 @@
 package com.group6.novadashboardbackend;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.boot.SpringApplication;
 
 /// The main NovaDashboardBackEndApplication tests.
-@SuppressWarnings("ClassNamePrefixedWithPackageName")
-@Slf4j
-@Testcontainers
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-final class NovaDashboardBackEndApplicationTests {
-  /// Mock PostgreSQL database
-  @Container
-  @ServiceConnection
-  private static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER =
-      new PostgreSQLContainer<>("postgres:17-alpine");
-
-  private NovaDashboardBackEndApplicationTests() {
+@SuppressWarnings({
+  "NonFinalUtilityClass",
+  "UtilityClassWithPublicConstructor",
+  "UtilityClassWithoutPrivateConstructor",
+  "ClassNamePrefixedWithPackageName",
+  "PMD.TestClassWithoutTestCases",
+  "PMD.UseUtilityClass"
+})
+public class NovaDashboardBackEndApplicationTests {
+  /// Constructor.
+  public NovaDashboardBackEndApplicationTests() {
     //
   }
 
-  @BeforeAll
-  static void beforeAll() {
-    POSTGRE_SQL_CONTAINER.start();
-
-    if (!POSTGRE_SQL_CONTAINER.isRunning()) {
-      throw new IllegalStateException("PostgreSQL container did not start properly");
-    }
-
-    final String jdbcUrl = POSTGRE_SQL_CONTAINER.getJdbcUrl();
-
-    log.info("PostgreSQL Container JDBC URL: {}", jdbcUrl);
-  }
-
-  @AfterAll
-  static void afterAll() {
-    POSTGRE_SQL_CONTAINER.stop();
-  }
-
-  @Test
-  void contextLoads() {
-    final int number = 1;
-    final int expected = 1;
-
-    assertThat(number).isEqualTo(expected);
+  /// The main test application entrypoint.
+  /// @param args the application arguments
+  public static void main(final String[] args) {
+    SpringApplication.from(NovaDashboardBackEndApplication::main)
+        .with(TestcontainersConfiguration.class)
+        .run(args);
   }
 }
