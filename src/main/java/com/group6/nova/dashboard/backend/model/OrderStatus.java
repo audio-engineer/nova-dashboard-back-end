ac23a9ec40f28d4dfe96bf567b101808ae48a359
Martin Kedmenec <martin.kedmenec@outlook.com>
2024-11-01 23:47:29 +0100

    Fix #44. Improve PaymentStatus and OrderStatus enums

--- src/main/java/com/group6/nova/dashboard/backend/model/OrderStatus.java
+++ src/main/java/com/group6/nova/dashboard/backend/model/OrderStatus.java
@@ -3,16 +3,20 @@ package com.group6.nova.dashboard.backend.model;
 import java.text.Collator;
 import java.util.Locale;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
 import lombok.ToString;
 
 /// Enum representing the various statuses an order can have.
 ///
 /// @author Martin Kedmenec
 @AllArgsConstructor
+@Getter
 @ToString
 public enum OrderStatus {
   /// Order is archived
   ARCHIVED("ARCHIVED"),
+
   /// Order is deleted/canceled
   DELETED("DELETED");
 
@@ -23,16 +27,20 @@ public enum OrderStatus {
   private final String status;
 
   /// Parses a string and returns the corresponding [PaymentStatus] enum constant.
+  ///
   /// @param status the payment status string
   /// @return the corresponding [PaymentStatus] enum constant
-  /// @throws IllegalArgumentException if the string does not match any constant
+  @NonNull
   public static OrderStatus fromString(final String status) {
     for (final OrderStatus orderStatus : values()) {
-      if (0 != COLLATOR.compare(orderStatus.status, status)) {
+      @SuppressWarnings("CallToSimpleGetterFromWithinClass")
+      final String statusValue = orderStatus.getStatus();
+
+      if (0 == COLLATOR.compare(statusValue, status)) {
         return orderStatus;
       }
     }
 
-    throw new IllegalArgumentException("Unknown order status: " + status);
+    throw new UnknownStatusException("Unknown order status: " + status);
   }
 }
